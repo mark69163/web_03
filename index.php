4750545e2ead5f80f4be1a561b54c2bb183a48c6
Kovács Márk <mark69163@gmail.com>
2024-03-10 14:51:10 +0100

    index.php added

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>Szorzotabla</title>
+</head>
+<body>
+    <h1>HEllo world</h1>
+    <table border="1">
+        <tr>
+            <td>1 * 1 = 1</td>
+            <td>1 * 2 = 2</td>
+            <td>1 * 3 = 3</td>
+            <td>1 * 4 = 4</td>
+            <td>1 * 5 = 5</td>
+            <td>1 * 6 = 6</td>
+            <td>1 * 7 = 7</td>
+            <td>1 * 8 = 8</td>
+            <td>1 * 9 = 9</td>
+            <td>1 * 10 = 10</td>
+
+        </tr>
+    </table>
+</body>
+</html>
